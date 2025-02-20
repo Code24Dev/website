18fa07dc7551115ec651374416973076e5176e45
Amani5576 <Amani5576@users.noreply.github.com>
2025-02-20 15:44:05 +0200

    Neatening up the navigation bar

--- navigation.js
+++ navigation.js
@@ -12,16 +12,14 @@ document.getElementById('navigation-container').innerHTML = `
 		.navbar-brand {
 			font-size: 20px;
 			padding-left: 5%;
-		}
-
-		.navbar-nav {
-			margin-right: auto;
-		}
-
+			}
+			
 		.nav-item {
 			font-size: 120%;
-			padding-left: 30px;
+			padding-left: 15px;
+			padding-right: 15px;
 			transition: all 0.5s ease; /* transition for smooth animation */
+			text-transform: uppercase;
 		}
 
 
@@ -30,60 +28,68 @@ document.getElementById('navigation-container').innerHTML = `
 			color: rgba(98, 42, 15, 1);
 		}
 
-		/* For now only being used for the logo at the top right corner */
+		/* For now only being used for the code lambda LOGO at the top right corner */
 		.icon-hover-msg {
 			display: none;
 			position: absolute;
-			background-color: rgba(33, 58, 92, 1);
+			background-color: transparent;
 			color: #fff;
 			z-index: 5;
 			padding: 2px 7px 2px 7px;
-			font-size: 30%;
+			left: 70%; /*With regards to the top right corner of the text*/
+			top: 50%; /*With regards to the top right corner of the text*/
+			font-size: 100%;
+			transform: translateY(-50%); /* Centering it vertically */
+    		white-space: nowrap; /* Preventing text from wrapping */
+			opacity: 0;
 		}
 
 		.nav-item:hover div{
       			display: block;
+				color: #fff;
+				opacity: 1;
     		}
   </style>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark nav-opacity fixed-top">
-    	
-	<nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top"> <!--navbar-expand-lg is breakponit, the bar close when reaching this size-->
-			<div class="container-fuild">    <!--fixed-top: ensures when clicking the button it stays on the directed content, but when scrolling up it does not position properly to fix that we adit the custom css file-->
-				
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+	<header>
+		<nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top nav-opacity"> <!--navbar-expand-lg is breakponit, the bar close when reaching this size-->
+				<div class="container-fluid">    <!--fixed-top: ensures when clicking the button it stays on the directed content, but when scrolling up it does not position properly to fix that we adit the custom css file-->
+					
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+						<div class="collapse navbar-collapse" id="navmenu">
+							<ul class="nav navbar-nav navbar-left">
+								<li class="nav-item">
+									<img src="img/code24_logo_zoomed_in.png" style="width: 3rem;" alt="Logo">
 
-					<div class="collapse navbar-collapse" id="navmenu">
-						<ul class="navbar-nav ms-auto"> <!--"ms-auto" is margin start, the aim is to shift nav-items to right-->
-							<li class="nav-item">
-								<img src="img/code24_logo_zoomed_in.png" style="width: 3rem;" alt="Logo">
+									<!-- When the mouse hovers on the image logo, it will show the user the name of the company-->
+									<div class="icon-hover-msg"> 
+										Code24
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="collapse navbar-collapse" id="navmenu">
+							<ul class="nav navbar-nav ms-auto"> <!--"ms-auto" is margin start, the aim is to shift nav-items to right-->
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="index.html">Home</a>
+								</li>
+								<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="#about">About</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="#services">Services</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="Contact_Me.html">Contacts</a>
+								</li>
+							</ul>
+						</div>
 
-								<!-- When the mouse hovers on the image logo, it will show the user the name of the company-->
-								<div class="icon-hover-msg"> 
-									Code24
-								</div>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link active" aria-current="page" href="index.html">Home</a>
-							</li>
-							<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#about">About</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#services">Services</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="Contact_Me.html">Contacts</a>
-						</li>
-
-					</ul>
-				</div>
-
-			</div>
-			
-		</nav>
+				</div>		
+			</nav>
+	</header>
 
 	<script>
 		document.addEventListener('DOMContentLoaded', function() {
@@ -93,9 +99,9 @@ document.getElementById('navigation-container').innerHTML = `
 
 			// Function to show the message at a certain position away from the cursor
 			function showMessage(message, e, x = 13, y = 13) {
-  				message.style.left = e.clientX + x + 'px';
-  				message.style.top = e.clientY + y + 'px';
-  				message.style.display = 'block';
+				message.style.left = e.clientX + x + 'px';
+				message.style.top = e.clientY + y + 'px';
+				message.style.display = 'block';
 			}
 
 			// Adding event listeners to all icons that are of class "nav-item"
@@ -117,11 +123,11 @@ document.getElementById('navigation-container').innerHTML = `
 				});
 
 				// When cursor moves ontop of the icon
-  				icon.addEventListener('mousemove', function (event) {
+				icon.addEventListener('mousemove', function (event) {
 
-    					// Update the message position as the cursor moves
+						// Update the message position as the cursor moves
 					showMessage(messages[index], event);
-  				});
+				});
 			});
 		});
 	</script>
